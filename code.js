2fd0c213e6385930e1418af84d47204aba3f4e15
Dmitry Verbovetskyi <verbovetskyi.d@gmail.com>
2021-01-25 04:02:21 +0200

    h/w updated

--- code.js
+++ code.js
@@ -1,51 +1,39 @@
 class Student {
-
-    #presentArr = new Array (30);
-    #markArr = new Array (30);
-
     constructor(studentName, studentSurname, studentDateOfBirth) {
         this.name = studentName;
         this.surname = studentSurname;
         this.dateOfBirth = studentDateOfBirth;
     }
 
-    static getAverage(arr) {
-        return arr.reduce( (acc, el) => acc + el ) / arr.length;
-    }
-
-    #arrIndexCheck(arr) {
-        return arr.findIndex( (el) => el === undefined);
-    }
+    #presentArr = new Array(30);
+    #markArr = new Array(30);
 
     getAge() {
-        const date = new Date;
+        const date = new Date();
         return date.getFullYear() - this.dateOfBirth;
     }
 
     present() {
         const index = this.#arrIndexCheck(this.#presentArr);
-        if (index !== -1)
-            this.#presentArr[index] = true;
+        if (index !== -1) this.#presentArr[index] = true;
     }
 
     absent() {
         const index = this.#arrIndexCheck(this.#presentArr);
-        if (index !== -1)
-            this.#presentArr[index] = false;
+        if (index !== -1) this.#presentArr[index] = false;
     }
 
     mark(mark) {
         const index = this.#arrIndexCheck(this.#markArr);
-        if (index !== -1 && mark >= 0 && mark <= 10)
-            this.#markArr[index] = mark;
+        if (index !== -1 && mark >= 0 && mark <= 10) this.#markArr[index] = mark;
     }
 
     averageMark() {
-        return Student.getAverage(this.#markArr);
+        return this.#getAverage(this.#markArr);
     }
 
     averagePresence() {
-        return Student.getAverage(this.#presentArr);
+        return this.#getAverage(this.#presentArr);
     }
 
     summary() {
@@ -62,18 +50,26 @@ class Student {
         return summary;
     }
 
+    #arrIndexCheck(arr) {
+        return arr.findIndex(el => el === undefined);
+    }
+
+    #getAverage(arr) {
+        return arr.reduce((acc, el) => acc + el) / arr.length;
+    }
 }
+
 // проверка
-const noName = new Student('NoName', 'NoSurname', 1995)
-noName.present()
-noName.absent()
-noName.present()
-noName.mark(10)
-noName.mark(55)
-noName.mark(0)
-noName.mark(5)
-console.log( noName.averageMark() )
-console.log( noName.averagePresence() )
-console.log( noName.getAge() )
-console.log( noName.summary() )
-console.log(noName)
+const noName = new Student('NoName', 'NoSurname', 1995);
+noName.present();
+noName.absent();
+noName.present();
+noName.mark(10);
+noName.mark(55);
+noName.mark(0);
+noName.mark(5);
+console.log(noName.averageMark());
+console.log(noName.averagePresence());
+console.log(noName.getAge());
+console.log(noName.summary());
+console.log(noName);
